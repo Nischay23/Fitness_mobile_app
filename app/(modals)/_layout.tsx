6b60735df301fdca7d725202ce36ac3afc02b5cc
unknown <khobragadenischay23@gmail.com>
2025-07-20 00:36:23 +0530

    refactoring old files

--- app/(modals)/_layout.tsx
+++ app/(modals)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 import React from "react";
 
-// This layout configures all screens in this group to be presentation modals.
 export default function ModalLayout() {
   return (
     <Stack
